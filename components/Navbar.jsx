b47c8438b913ca87448851e69942b724ceb6d73d
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-25 23:45:38 +0100

    navbar section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,52 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+           <Image src='/../public/assets/logo.png' width={125} height={50} alt='/' className='md:ms-5' />
+           <div>
+            <ul className='hidden md:flex mr-5'>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                </Link>
+                <Link href='/'>
+                    <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                </Link>
+            </ul>
+            <div className='md:hidden'>
+                <AiOutlineMenu size={25} className='text-purple-600'/>
+            </div>
+           </div>
+         </div>
+
+         <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+            <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] py-5 px-3 ease-in duration-500'>
+             <div>
+                <div className='flex w-full items-center justify-between'>
+                    <Image src="/../public/assets/logo.png" alt='/' width={87} height={35} />
+                    <div>
+                          <AiOutlineClose className='text-purple-600' />
+                    </div>
+                </div>
+
+             </div>
+            </div>
+
+         </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import Head from 'next/head'
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <title>Elijah Aremu</title>
    </Head>
    <main>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam illum natus expedita pariatur vitae dignissimos nostrum, consectetur voluptate veritatis possimus voluptates omnis sunt reiciendis provident suscipit adipisci iusto cum necessitatibus. Molestiae perspiciatis quasi doloremque dolorem quisquam fugit reprehenderit, corporis debitis nisi animi, eius quas eligendi praesentium sapiente architecto minima commodi.
+    <Navbar />
    </main>
    </>
   )

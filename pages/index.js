b3b5038497e4aeefccd2d22212c4669920087d65
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-25 22:40:34 +0100

    Project initialization

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+   <>
+   <Head>
+    <title>Elijah Aremu</title>
+   </Head>
+   <main>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam illum natus expedita pariatur vitae dignissimos nostrum, consectetur voluptate veritatis possimus voluptates omnis sunt reiciendis provident suscipit adipisci iusto cum necessitatibus. Molestiae perspiciatis quasi doloremque dolorem quisquam fugit reprehenderit, corporis debitis nisi animi, eius quas eligendi praesentium sapiente architecto minima commodi.
+   </main>
+   </>
+  )
+}
